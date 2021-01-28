bcaebe8f5cb35bf3d919292548f97d7e37342b4f
Daniel Chivu <daniel@quickweb.ro>
2021-01-28 23:18:44 +0200

    Start sqlite implementation

--- lib/ui/entities/category.dart
+++ lib/ui/entities/category.dart
@@ -1,9 +1,9 @@
 import 'package:experiment/ui/entities/category_type.dart';
 
 class Category{
-  int id;
-  String name;
-  CategoryType type;
+  final int id;
+  final String name;
+  final CategoryType type;
 
   Category(this.id, this.name, this.type);
 }
--- lib/ui/entities/category_type.dart
+++ lib/ui/entities/category_type.dart
@@ -1,7 +1,7 @@
 class CategoryType {
-  int id;
-  String tag;
-  String name;
+  final int id;
+  final String tag;
+  final String name;
 
   CategoryType(this.id, this.tag, this.name);
 }
