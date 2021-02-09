189cc27c1b9cb34510913ed1bc14528cb0e8317c
Daniel Chivu <daniel@quickweb.ro>
2021-02-09 15:28:37 +0200

    Transactions list

--- lib/services/OperationsService.dart
+++ lib/services/OperationsService.dart
@@ -1,3 +1,5 @@
+import 'package:experiment/entities/category.dart';
+import 'package:experiment/entities/category_type.dart';
 import 'package:experiment/entities/operation.dart';
 import 'package:experiment/services/DatabaseProvider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -23,5 +25,54 @@ class OperationsService {
     var incomeSum = income.first['sum'];
     var expense = expenses.first['sum'];
     return incomeSum - expense;
+  }
+
+  Future<List<Operation>> getBetween() async {
+    //TODO Remote hardcoded
+    DateTime now = DateTime.now();
+    DateTime firstDayOfMonth = DateTime(now.year, now.month);
+    DateTime lastDayOfMonth = DateTime(now.year, now.month + 1);
+    Database db = await DatabaseProvider().database;
+
+    List<Map> list = await db.rawQuery(
+        "SELECT  operations.id, "
+        "operations.amount, "
+        "operations.created_at, "
+        "operations.category_id, "
+        "categories.name, "
+        "categories.icon, "
+        "categories.color, "
+        "categories.type"
+        " FROM operations "
+        "JOIN categories ON operations.category_id = categories.id "
+        "WHERE operations.created_at BETWEEN ? AND ? "
+            "ORDER BY operations.created_at DESC",
+        [
+          firstDayOfMonth.millisecondsSinceEpoch,
+          lastDayOfMonth.millisecondsSinceEpoch
+        ]);
+    Map categories = {};
+    List<Operation> operations = [];
+    for (var item in list) {
+      Category category;
+      if (categories.containsKey(item['category_id'])) {
+        category = categories[item['category_id']];
+      } else {
+        category = Category(
+            id: item['category_id'],
+            name: item['name'],
+            icon: item['icon'],
+            color: item['color'],
+            type: getCategoryTypeByTag(item['type']));
+        categories[item['category_id']] = category;
+      }
+      Operation operation = Operation(
+          id: item['id'],
+          amount: item['amount'],
+          createdAt: item['created_at'],
+          category: category);
+      operations.add(operation);
+    }
+    return operations;
   }
 }
---
+++ lib/ui/home/balance.dart
@@ -0,0 +1,56 @@
+import 'package:experiment/services/OperationsService.dart';
+import 'package:flutter/material.dart';
+
+class BalanceWidget extends StatefulWidget {
+  @override
+  _BalanceWidgetState createState() => _BalanceWidgetState();
+}
+
+class _BalanceWidgetState extends State<BalanceWidget> {
+  double totalBalance = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    OperationsService().getTotalBalance().then((value) => {
+      setState(() {
+        totalBalance = value;
+      })
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  child: Text(
+                    'Your balance',
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  child: Text(
+                    totalBalance.toStringAsFixed(2) + ' lei',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
