190c612d6a49cabd1b9a50b6bed0ef8643a28c04
Daniel Chivu <daniel@quickweb.ro>
2021-02-18 22:30:52 +0200

    Add exchange button

--- lib/ui/home/balance.dart
+++ lib/ui/home/balance.dart
@@ -1,5 +1,6 @@
 import 'package:experiment/services/OperationsService.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BalanceWidget extends StatefulWidget {
   @override
@@ -23,76 +24,108 @@ class _BalanceWidgetState extends State<BalanceWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Stack(
       children: [
-        Flexible(
-          flex: 1,
-          child: Card(
-            child: Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
+        Row(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Card(
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Container(
-                        width: 100,
-                        child: Text(
-                          'Your balance',
-                          style: TextStyle(fontSize: 14, color: Colors.grey),
-                        ),
+                      Row(
+                        children: [
+                          Container(
+                            width: 100,
+                            child: Text(
+                              'Your balance',
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            child: Text(
+                              totalBalance.toStringAsFixed(2) + ' lei',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          )
+                        ],
                       ),
                     ],
                   ),
-                  Row(
-                    children: [
-                      Container(
-                        child: Text(
-                          totalBalance.toStringAsFixed(2) + ' lei',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-        if(savings > 0) Flexible(
-          flex: 1,
-          child: Card(
-            child: Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 100,
-                        child: Text(
-                          'Your savings',
-                          style: TextStyle(fontSize: 14, color: Colors.grey),
+            if (savings > 0)
+              Flexible(
+                flex: 1,
+                child: Card(
+                  child: Container(
+                    padding: EdgeInsets.only(
+                        left: 30, right: 20, top: 20, bottom: 20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 100,
+                              child: Text(
+                                'Your savings',
+                                style:
+                                    TextStyle(fontSize: 14, color: Colors.grey),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        child: Text(
-                          savings.toStringAsFixed(2) + ' lei',
-                          style: TextStyle(fontSize: 20),
+                        Row(
+                          children: [
+                            Container(
+                              child: Text(
+                                savings.toStringAsFixed(2) + ' lei',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
+          ],
         ),
+        if (savings > 0)
+          Align(
+              heightFactor: 2.5,
+              alignment: Alignment.center,
+              child: SizedBox(
+                height: 35,
+                width: 35,
+                child: Container(
+                  padding: EdgeInsets.all(0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      // borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Colors.green),
+                  child: IconButton(
+                      focusColor: Colors.grey,
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      padding: EdgeInsets.all(0),
+                      iconSize: 20,
+                      color: Colors.white,
+                      icon: Icon(Icons.west),
+                      onPressed: () {}),
+                ),
+              )),
       ],
     );
   }
