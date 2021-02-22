a0c5560aeeea0d46bc98ab7edd154ec58d633142
Daniel Chivu <daniel@quickweb.ro>
2021-02-22 15:24:35 +0200

    Basic tests

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,47 +1,40 @@
 import 'package:experiment/main.dart';
 import 'package:experiment/ui/home/balance.dart';
-import 'package:experiment/ui/transactions/add_amount.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'actions/add_operations.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Show home screen', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
-    expect(find.text('Your balance'), findsOneWidget);
-    expect(find.text('0.00 lei'), findsOneWidget);
-    expect(find.text('Transaction list'), findsOneWidget);
-    expect(find.text('There are no operations yet..'), findsOneWidget);
+    await showHomeScreen(tester, '0.00');
   });
 
   testWidgets('Show category types', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-    expect(find.text('Select category'), findsOneWidget);
-    expect(find.text('EXPENSE'), findsOneWidget);
-    expect(find.text('INCOME'), findsOneWidget);
-    expect(find.text('SAVINGS'), findsOneWidget);
+    await showHomeScreen(tester, '0.00');
+    await showCategoryTypesScreen(tester);
   });
 
   testWidgets('Add salary', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('INCOME'));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('Salary'));
-    await tester.pumpAndSettle();
-    expect(find.text('Add Salary amount'), findsOneWidget);
-    await tester.tap(find.widgetWithText(CalculatorKey, '1'));
-    await tester.tap(find.widgetWithText(CalculatorKey, '0'));
-    await tester.tap(find.widgetWithText(CalculatorKey, '0'));
-    await tester.tap(find.widgetWithText(CalculatorKey, '0'));
-    await tester.tap(find.byIcon(Icons.done));
-    await tester.pumpAndSettle();
-    expect(find.widgetWithText(ListTile, '1000.00 lei'), findsOneWidget);
-    expect(find.widgetWithText(BalanceWidget, '1000.00 lei'), findsOneWidget);
+    await showHomeScreen(tester, '0.00');
+    await showCategoryTypesScreen(tester);
+    await addIncome(tester, 'Salary', '1000');
+    await showHomeScreen(tester, '1000.00', transactionList: ['1000.00 lei']);
+  });
+
+  testWidgets('Add expense', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await showHomeScreen(tester, '0.00');
+    await showCategoryTypesScreen(tester);
+    await addExpense(tester, 'Home', '500');
+    await showHomeScreen(tester, '500.00', transactionList: [
+      '1000.00 lei',
+      '500.00 lei'
+    ]);
   });
 }
