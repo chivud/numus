151177194e2987179a0498bcad0f5f1262b80281
Daniel Chivu <daniel@quickweb.ro>
2021-03-10 10:58:02 +0200

    Currencies list

---
+++ lib/constants/currencies.dart
@@ -0,0 +1,82 @@
+const Map currencies = {
+  'us_dollar': {
+    'display_name' : 'United States dollar',
+    'symbol' : '\$'
+  },
+  'euro': {
+    'display_name' : 'Euro',
+    'symbol' : '€'
+  },
+  'jpn_yen': {
+    'display_name' : 'Japanese yen',
+    'symbol' : '¥'
+  },
+  'pound': {
+    'display_name' : 'Pound sterling',
+    'symbol' : '£'
+  },
+  'aus_dollar': {
+    'display_name' : 'Australian dollar',
+    'symbol' : '\$'
+  },
+  'cad_dollar': {
+    'display_name' : 'Canadian dollar',
+    'symbol' : '\$'
+  },
+  'sw_franc': {
+    'display_name' : 'Swiss franc',
+    'symbol' : 'CHF'
+  },
+  'renminbi': {
+    'display_name' : 'China yuan',
+    'symbol' : '¥'
+  },
+  'hk_dollar': {
+    'display_name' : 'Hong Kong dollar',
+    'symbol' : '\$'
+  },
+  'nz_dollar': {
+    'display_name' : 'New Zealand dollar',
+    'symbol' : '\$'
+  },
+  'sw_korona': {
+    'display_name' : 'Swedish krona',
+    'symbol' : 'kr'
+  },
+  'sk_won': {
+    'display_name' : 'South Korean won',
+    'symbol' : '₩'
+  },
+  'sin_dollar': {
+    'display_name' : 'Singapore dollar',
+    'symbol' : '\$'
+  },
+  'nw_krone': {
+    'display_name' : 'Norwegian krone',
+    'symbol' : 'kr'
+  },
+  'peso': {
+    'display_name' : 'Mexican peso',
+    'symbol' : '\$'
+  },
+  'rupee': {
+    'display_name' : 'Indian rupee',
+    'symbol' : '₹'
+  },
+  'ruble': {
+    'display_name' : 'Russian ruble',
+    'symbol' : '₽'
+  },
+  'rand': {
+    'display_name' : 'South African rand',
+    'symbol' : 'R'
+  },
+  'tk_lira': {
+    'display_name' : 'Turkish lira',
+    'symbol' : '₺'
+  },
+  'leu': {
+    'display_name' : 'Romanian leu',
+    'symbol' : 'lei'
+  },
+};
