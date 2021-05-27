aa72485fc9e512ab17c137bad31e08bec547918f
Daniel Chivu <daniel@quickweb.ro>
2021-05-27 11:22:50 +0300

    Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:numus/entities/settings.dart';
@@ -6,11 +8,19 @@ import 'package:numus/services/SharedPreferencesService.dart';
 import 'package:numus/ui/home/home_screen.dart';
 import 'package:numus/ui/wizard/get_started.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 
 import 'constants/application.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (kDebugMode) {
+    // Force disable Crashlytics collection while doing every day development.
+    // Temporarily toggle this to true if you want to test crash reporting in your app.
+    await FirebaseCrashlytics.instance
+        .setCrashlyticsCollectionEnabled(false);
+  }
   await DatabaseProvider().database;
   Settings settings = await SharedPreferencesService().getSettings();
   Widget first;
